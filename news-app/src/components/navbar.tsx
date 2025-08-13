d77dfdc208e179d5ba910ce3f7f64084d10f41df
Riitha <myuuta29.00@gmail.com>
2025-08-13 20:56:38 +0700

    add navbar

--- news-app/src/components/navbar.tsx
+++ news-app/src/components/navbar.tsx
@@ -3,10 +3,14 @@ import Link from "next/link"
 export default function Navbar() {
     return (
         <>
-        <div className="flex gap-4">
-            <Link href='/'>Home</Link>
-            <Link href='/about'>About</Link>
-        </div>
+            <div className="navbar bg-base-100 shadow-sm">
+                <div className="flex-1">
+                    <Link href='/' className="btn btn-ghost text-xl">NHK</Link>
+                </div>
+                <div className="flex gap-2">
+                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                </div>
+            </div>
         </>
     )
 }
